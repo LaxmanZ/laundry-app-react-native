b7fbe1402d6f7da2e8773cd2c6e222ad1a69be9b
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-10-17 12:44:27 +0530

    updated the PickUpScreen and StackNavigator

---
+++ screens/CartScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+
+const CartScreen = () => {
+  return (
+    <SafeAreaView style={{ marginTop: 40 }}>
+      <Text>CartScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default CartScreen;
--- screens/PickUpScreen.js
+++ screens/PickUpScreen.js
@@ -5,14 +5,21 @@ import {
   StyleSheet,
   ScrollView,
   Pressable,
+  Alert,
 } from 'react-native';
 import React, { useState } from 'react';
 import { TextInput } from 'react-native';
 import HorizontalDatepicker from '@awrminkhodaei/react-native-horizontal-datepicker';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
 
 const PickUpScreen = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  // console.log(cart);
+  const total = cart
+    .map((item) => item.quantity * item.price)
+    .reduce((curr, prev) => curr + prev, 0);
   const [selectedDate, setSelectedDate] = useState('');
   const [selectedTime, setSelectedTime] = useState([]);
   const [dalevary, setDalevary] = useState([]);
@@ -67,117 +74,181 @@ const PickUpScreen = () => {
     },
   ];
 
+  const proceedToCart = () => {
+    if (!selectedDate || !selectedTime || !dalevary) {
+      Alert.alert(
+        'Empty or Invalid',
+        'Please Select all the Fields',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancle Pressed'),
+            style: 'cancel',
+          },
+          { text: 'OK', onPress: () => console.log('Ok Pressed') },
+        ],
+        { cancelable: false }
+      );
+    }
+
+    if (selectedDate && selectedTime && dalevary) {
+      navigation.replace('Cart');
+    }
+  };
+
   return (
-    <SafeAreaView style={{ marginTop: 40 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          marginRight: 8,
-        }}
-      >
+    <>
+      <SafeAreaView style={{ marginTop: 40 }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginRight: 8,
+          }}
+        >
+          <Text
+            style={{ fontSize: 16, fontWeight: '500', marginHorizontal: 10 }}
+          >
+            Enter Address
+          </Text>
+          <Pressable onPress={() => navigation.navigate('Home')}>
+            <Ionicons name="arrow-back" size={28} color="#fd5c63" />
+          </Pressable>
+        </View>
+        <TextInput
+          style={{
+            padding: 40,
+            borderColor: 'gray',
+            borderWidth: 0.7,
+            paddingVertical: 50,
+            borderRadius: 8,
+            margin: 10,
+          }}
+        />
         <Text style={{ fontSize: 16, fontWeight: '500', marginHorizontal: 10 }}>
-          Enter Address
+          Pick Up Date
         </Text>
-        <Pressable onPress={() => navigation.navigate('Home')}>
-          <Ionicons name="arrow-back" size={28} color="#fd5c63" />
+
+        <HorizontalDatepicker
+          mode="gregorian"
+          startDate={new Date('2023-02-21')}
+          endDate={new Date('2023-02-28')}
+          initialSelectedDate={new Date('2020-08-22')}
+          onSelectedDateChange={(date) => setSelectedDate(date)}
+          selectedItemWidth={170}
+          unselectedItemWidth={38}
+          itemHeight={38}
+          itemRadius={10}
+          selectedItemTextStyle={styles.selectedItemTextStyle}
+          unselectedItemTextStyle={styles.selectedItemTextStyle}
+          selectedItemBackgroundColor="#222831"
+          unselectedItemBackgroundColor="#ececec"
+          flatListContainerStyle={styles.flatListContainerStyle}
+        />
+
+        <Text style={{ fontSize: 16, fontWeight: '500', marginHorizontal: 10 }}>
+          Select Time
+        </Text>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {times.map((item, index) => (
+            <Pressable
+              key={index}
+              onPress={() => setSelectedTime(item.time)}
+              style={
+                selectedTime.includes(item.time)
+                  ? {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 12,
+                      borderColor: 'red',
+                      borderWidth: 0.7,
+                    }
+                  : {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 12,
+                      borderColor: 'gray',
+                      borderWidth: 0.7,
+                    }
+              }
+            >
+              <Text>{item.time}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+
+        <Text style={{ fontSize: 16, fontWeight: '500', marginHorizontal: 10 }}>
+          Delivery Date
+        </Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {deliveryTime.map((item, i) => (
+            <Pressable
+              key={i}
+              onPress={() => setDalevary(item.name)}
+              style={
+                dalevary.includes(item.name)
+                  ? {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 12,
+                      borderColor: 'red',
+                      borderWidth: 0.7,
+                    }
+                  : {
+                      margin: 10,
+                      borderRadius: 7,
+                      padding: 12,
+                      borderColor: 'gray',
+                      borderWidth: 0.7,
+                    }
+              }
+            >
+              <Text>{item.name}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+
+      {total === 0 ? null : (
+        <Pressable
+          style={{
+            marginTop: 'auto',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            backgroundColor: '#088f8f',
+            padding: 10,
+            // marginBottom: 10,
+            margin: 15,
+            borderRadius: 7,
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 17, fontWeight: '600', color: 'white' }}>
+              {cart.length} items | total $ {total}
+            </Text>
+            <Text
+              style={{
+                fontSize: 15,
+                fontWeight: '400',
+                color: 'white',
+                marginVertical: 6,
+              }}
+            >
+              Extra charges might Apply
+            </Text>
+          </View>
+
+          <Pressable onPress={proceedToCart}>
+            <Text style={{ fontSize: 17, fontWeight: '600', color: 'white' }}>
+              Proceed To Cart
+            </Text>
+          </Pressable>
         </Pressable>
-      </View>
-      <TextInput
-        style={{
-          padding: 40,
-          borderColor: 'gray',
-          borderWidth: 0.7,
-          paddingVertical: 50,
-          borderRadius: 8,
-          margin: 10,
-        }}
-      />
-      <Text style={{ fontSize: 16, fontWeight: '500', marginHorizontal: 10 }}>
-        Pick Up Date
-      </Text>
-
-      <HorizontalDatepicker
-        mode="gregorian"
-        startDate={new Date('2023-02-21')}
-        endDate={new Date('2023-02-28')}
-        initialSelectedDate={new Date('2020-08-22')}
-        onSelectedDateChange={(date) => setSelectedDate(date)}
-        selectedItemWidth={170}
-        unselectedItemWidth={38}
-        itemHeight={38}
-        itemRadius={10}
-        selectedItemTextStyle={styles.selectedItemTextStyle}
-        unselectedItemTextStyle={styles.selectedItemTextStyle}
-        selectedItemBackgroundColor="#222831"
-        unselectedItemBackgroundColor="#ececec"
-        flatListContainerStyle={styles.flatListContainerStyle}
-      />
-
-      <Text style={{ fontSize: 16, fontWeight: '500', marginHorizontal: 10 }}>
-        Select Time
-      </Text>
-
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {times.map((item, index) => (
-          <Pressable
-            key={index}
-            onPress={() => setSelectedTime(item.time)}
-            style={
-              selectedTime.includes(item.time)
-                ? {
-                    margin: 10,
-                    borderRadius: 7,
-                    padding: 12,
-                    borderColor: 'red',
-                    borderWidth: 0.7,
-                  }
-                : {
-                    margin: 10,
-                    borderRadius: 7,
-                    padding: 12,
-                    borderColor: 'gray',
-                    borderWidth: 0.7,
-                  }
-            }
-          >
-            <Text>{item.time}</Text>
-          </Pressable>
-        ))}
-      </ScrollView>
-
-      <Text style={{ fontSize: 16, fontWeight: '500', marginHorizontal: 10 }}>
-        Delivery Date
-      </Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {deliveryTime.map((item, i) => (
-          <Pressable
-            key={i}
-            onPress={() => setDalevary(item.name)}
-            style={
-              dalevary.includes(item.name)
-                ? {
-                    margin: 10,
-                    borderRadius: 7,
-                    padding: 12,
-                    borderColor: 'red',
-                    borderWidth: 0.7,
-                  }
-                : {
-                    margin: 10,
-                    borderRadius: 7,
-                    padding: 12,
-                    borderColor: 'gray',
-                    borderWidth: 0.7,
-                  }
-            }
-          >
-            <Text>{item.name}</Text>
-          </Pressable>
-        ))}
-      </ScrollView>
-    </SafeAreaView>
+      )}
+    </>
   );
 };
 
